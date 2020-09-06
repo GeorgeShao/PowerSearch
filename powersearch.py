3f6b86d5a7f890da3f2b0b6defb9810a41040909
George Shao <georgeshao123@gmail.com>
2020-09-06 17:08:39 -0400

    Fixed multiple filename bugs

--- powersearch.py
+++ powersearch.py
@@ -10,18 +10,14 @@ args = parser.parse_args()
 
 std_ignored_exts = [".cache", ".pyc"]
 
+valid_dir_name = True
+
 def listfiles(path):
     if path == "" or path == None:
         path = os.getcwd()
     print(f'PATH = {args.path}')
-    
+
     files = []
-
-    num_skipped_dot_dirs = 0
-    num_skipped_dot_files = 0
-    num_skipped_noext_files = 0
-    num_skipped_stdignored_files = 0
-
     skipped_dot_dirs = []
     skipped_dot_files = []
     skipped_noext_files = []
@@ -29,43 +25,49 @@ def listfiles(path):
     
     # r=root, d=directories, f=files
     for r, d, f in os.walk(path):
-        # check if the folder is hidden
+        # check if dir is a dot dir
         for dir in d:
-            if not dir.startswith(".") and not args.include_dot_dirs:
-                for filename in f:
-                    hide_file_status = False
-                    # check if the file only has an extension and no name
-                    if filename.startswith(".") and not args.include_dot_files:
-                        num_skipped_dot_files += 1
+            if dir.startswith(".") and not args.include_dot_dirs:
+                skipped_dot_dirs.append(dir)
+                valid_dir_name = False
+            else:
+                valid_dir_name = True
+        if valid_dir_name:
+            for filename in f:
+                # print(r, d, f, filename)
+                hide_file_status = False
+                # check if the file only has an extension and no name
+                if filename.startswith(".") and not args.include_dot_files:
+                    if filename not in skipped_dot_files:
                         skipped_dot_files.append(filename)
                         hide_file_status = True
-                        break
-                    # check if filename doesn't have an extension
-                    if "." not in filename and not args.include_no_ext:
-                        num_skipped_noext_files += 1
+                    continue
+                # check if filename doesn't have an extension
+                if "." not in filename and not args.include_no_ext:
+                    if filename not in skipped_noext_files:
                         skipped_noext_files.append(filename)
                         hide_file_status = True
-                        break
-                    # check if filename has a standard ignored extension
-                    for ext in std_ignored_exts:
-                        if filename.endswith(ext):
+                    continue
+                # check if filename has a standard ignored extension
+                for ext in std_ignored_exts:
+                    if filename.endswith(ext):
+                        if filename not in skipped_stdignored_files:
                             hide_file_status = True
-                            num_skipped_stdignored_files += 1
                             skipped_stdignored_files.append(filename)
-                            break
-                    # output filenames that meet all criteria
-                    if not hide_file_status:
-                        files.append(os.path.join(r, filename))
-                        print(filename)
-            else:
-                num_skipped_dot_dirs += 1
-                skipped_dot_dirs.append(dir)
+                        break
+                # output filenames that meet all criteria
+                if not hide_file_status:
+                    # files.append(os.path.join(r, filename))
+                    if filename not in files:
+                        files.append(filename)
+                
     # output skipped dirs/files stats
-    print(f"SKIPPED DOT DIRS = {num_skipped_dot_dirs} {skipped_dot_dirs}")
-    print(f"SKIPPED DOT FILES = {num_skipped_dot_files} {skipped_dot_files}")
-    print(f"SKIPPED NOEXT FILES = {num_skipped_noext_files} {skipped_noext_files}")
-    print(f"SKIPPED STDIGNORED EXTS = {num_skipped_stdignored_files} {skipped_stdignored_files}")
-    
+    print(f"SKIPPED DOT DIRS = {len(skipped_dot_dirs)} {skipped_dot_dirs}\n")
+    print(f"SKIPPED DOT FILES = {len(skipped_dot_files)} {skipped_dot_files}\n")
+    print(f"SKIPPED NOEXT FILES = {len(skipped_noext_files)} {skipped_noext_files}\n")
+    print(f"SKIPPED STDIGNORED EXTS = {len(skipped_stdignored_files)} {skipped_stdignored_files}\n")
+    print(f"FILES = {len(files)} {files}\n")
+
 
 listfiles(args.path)
 
