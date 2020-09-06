7ded39ed9f3d4185d15d81008e6ab33cdfed2571
George Shao <georgeshao123@gmail.com>
2020-09-06 11:27:05 -0400

    Added dir filename listing & custom paths

--- powersearch.py
+++ powersearch.py
@@ -5,13 +5,13 @@ import argparse
 # logging.basicConfig(level=logging.INFO)
 
 parser = argparse.ArgumentParser(description='Command line tool for searching the content of multiple files at once')
-parser.add_argument('--a', help='help_text_a')
+parser.add_argument('--path', help='Select a path')
 parser.add_argument('--b', action='store_true', help='help_text_b')
 parser.add_argument('--c', action='store_true', help='help_text_c')
 args = parser.parse_args()
 
-if args.a:
-    logging.info('--a enabled. Accounting will not be disabled.')
+if args.path:
+    logging.info(f'path={args.path}')
 
 if args.b:
     temp_a = "option_a"
@@ -24,6 +24,19 @@ else:
     temp_b = "option_d"
 
 # logging.info(f"stuff: {temp_a,temp_b}")
-print(f"stuff: {temp_a,temp_b}")
+# print(f"stuff: {temp_a,temp_b}")
+
+def listfiles(path):
+    if path == "" or path == None:
+        path = os.getcwd()
+    print(path)
+    files = []
+    # r=root, d=directories, f=files
+    for r, d, f in os.walk(path):
+        for filename in f:
+            files.append(os.path.join(r, filename))
+            print(filename)
+
+listfiles(args.path)
 
 k = input("Finished.") 
