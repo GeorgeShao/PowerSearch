68b30baa382c2dae4705aceb9caa6f2e954e152a
George Shao <georgeshao123@gmail.com>
2020-09-07 13:54:31 -0400

    Fixed more major dir/file detection bugs

--- powersearch.py
+++ powersearch.py
@@ -8,9 +8,7 @@ parser.add_argument('--include-dot-files', action='store_true', help='Include fi
 parser.add_argument('--include-no-ext', action='store_true', help='Include files with no extension')
 args = parser.parse_args()
 
-std_ignored_exts = [".cache", ".pyc", ".sample"]
-
-valid_dir_name = True
+std_ignored_exts = []
 
 def listfiles(path):
     if path == "" or path == None:
@@ -29,38 +27,43 @@ def listfiles(path):
         for dir in d:
             if dir.startswith(".") and not args.include_dot_dirs:
                 skipped_dot_dirs.append(dir)
-                valid_dir_name = False
-            else:
-                valid_dir_name = True
-        if valid_dir_name:
-            for filename in f:
-                # print(r, d, f, filename)
-                hide_file_status = False
-                # check if the file only has an extension and no name
-                if filename.startswith(".") and not args.include_dot_files:
-                    if filename not in skipped_dot_files:
-                        skipped_dot_files.append(filename)
+        for filename in f:
+            # check if file is in a dot dir
+            full_file_path = os.path.join(r, filename)
+            in_dot_dir = False
+            for dir in skipped_dot_dirs:
+                if ("\\" + dir + "\\") in full_file_path:
+                    in_dot_dir = True
+                    break
+                if ("/" + dir + "/") in full_file_path:
+                    in_dot_dir = True
+                    break
+            if in_dot_dir:
+                continue
+            hide_file_status = False
+            # check if the file only has an extension and no name
+            if filename.startswith(".") and not args.include_dot_files:
+                if filename not in skipped_dot_files:
+                    skipped_dot_files.append(filename)
+                continue
+            # check if filename doesn't have an extension
+            if "." not in filename and not args.include_no_ext:
+                if filename not in skipped_noext_files:
+                    skipped_noext_files.append(filename)
+                continue
+            # check if filename has a standard ignored extension
+            for ext in std_ignored_exts:
+                if filename.endswith(ext):
+                    if filename not in skipped_stdignored_files:
                         hide_file_status = True
-                    continue
-                # check if filename doesn't have an extension
-                if "." not in filename and not args.include_no_ext:
-                    if filename not in skipped_noext_files:
-                        skipped_noext_files.append(filename)
-                        hide_file_status = True
-                    continue
-                # check if filename has a standard ignored extension
-                for ext in std_ignored_exts:
-                    if filename.endswith(ext):
-                        if filename not in skipped_stdignored_files:
-                            hide_file_status = True
-                            skipped_stdignored_files.append(filename)
-                        break
-                # output filenames that meet all criteria
-                if not hide_file_status:
-                    # if filename not in files:
-                    #     files.append(filename)
-                    if os.path.join(r, filename) not in files:
-                        files.append(os.path.join(r, filename))
+                        skipped_stdignored_files.append(filename)
+                    break
+            # output filenames that meet all criteria
+            if not hide_file_status:
+                # if filename not in files:
+                #     files.append(filename)
+                if full_file_path not in files:
+                    files.append(full_file_path)
                 
     # output skipped dirs/files stats
     print(f"SKIPPED DOT DIRS = {len(skipped_dot_dirs)} {skipped_dot_dirs}\n")
